a8c351e889e1cc23535af0f9662306f5179f2fb5
Guillermo Garcia <ggcobo806@gmail.com>
2020-04-11 21:17:44 +0200

    Use User class

--- discovery_server.py
+++ discovery_server.py
@@ -1,5 +1,6 @@
 import socket
-import requests
+from typing import List
+from user import User
 
 BUFFER_SIZE = 1024  # TODO: ajustar más si se quiere
 server_hostname = 'tfg.eps.uam.es'
@@ -24,7 +25,7 @@ def _send(message: bytes) -> str:
     :param message: message encoded in bytes to be sent
     :return: response of the server TODO: type
     """
-    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as connection:
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as connection:  # TODO: organizar según conveniencia
         connection.connect((socket.gethostbyname(server_hostname), server_port))
         connection.send(message)
         response = connection.recv(BUFFER_SIZE)
@@ -32,34 +33,24 @@ def _send(message: bytes) -> str:
     return str(response)
 
 
-def _get_public_IP():  # TODO
+def register(user: User, password: str):  # TODO: incluir password en User si se necesita en otros sitios
     """
-    :return: own public IP
-    """
-    return requests.get('http://ip.42.pl/raw').text
-
-
-def register(nick: str, port: int, password: str, protocols: str):
-    """
-    Registers a user in the system with the specified parameters
-    :param nick
-    :param port
+    Registers the user in the system with the specified parameters
+    :param user
     :param password
-    :param protocols
     :raise RegisterFailed
     """
-    ip = _get_public_IP()
-    string_to_send = f"REGISTER {nick} {ip} {port} {password} {protocols}"
+    string_to_send = f"REGISTER {user.nick} {user.ip} {user.tcp_port} {password} {user.protocols}"
     response = _send(string_to_send.encode()).split()
     if response[0] == "NOK":
         raise RegisterFailed
 
 
-def get_user(nick: str) -> list:
+def get_user(nick: str) -> User:
     """
     Gets the IP, port and protocols of the user with the specified nickname
     :param nick
-    :return: [ip, port, protocols] TODO: devolvemos también el nick? Ya lo tenemos así que para que no?
+    :return: User
     :raise UserUnknown if user is not found
     """
     string_to_send = f"QUERY {nick}"
@@ -67,22 +58,16 @@ def get_user(nick: str) -> list:
     if response[0] == "NOK":
         raise UserUnknown(nick)
     else:
-        return response[2:]
+        return User(nick, ip=response[2], tcp_port=int(response[3]), protocols=response[4])
 
 
-def list_users() -> list:
+def list_users() -> List[User]:
     """
     Gets a list of all the users
-    :return: list of users. Each user is: [nick, ip, port, protocols]
+    :return: list of users.
     """
-    return _send("LIST_USERS".encode()).split('#')  # TODO: de verdad esto puede devolver NOK?
+    return [User(nick=response[0], ip=response[1], tcp_port=int(response[2]), protocols=response[3])
+            for response in (_send("LIST_USERS".encode()).split()[2]).split('#')]  # TODO: de verdad esto puede devolver NOK?
 
 
-def close_connection():
-    """
-    Let the server know that we are closing the connection TODO: pero si los sockets los cerramos cada vez, esto pa que? Y tener un socket general abierto creo que no tendría nada de sentido, porque seguramente salte el timeout
-    """
-    _send("QUIT".encode())
-
-
-print(register("Dani", 8080, "secret", "V0"))
+print(register(User("dani", "V0", 8080), "secret"))
