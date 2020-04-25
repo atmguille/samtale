532ba892297b223260e8238109d5001bfe17db71
Guillermo Garcia <ggcobo806@gmail.com>
2020-04-26 01:05:17 +0200

    Solve blocking problems with semaphores

--- call_control.py
+++ call_control.py
@@ -38,7 +38,7 @@ class CallControl:
         self.__listener_stop = False
         # These two variables are needed in case both of the users hold the call.
         # Only if both of them are not in "hold", the call can continue
-        self.own_hold = False  # If call is held by us
+        self.own_hold = True  # If call is held by us
         self.foreign_hold = False  # If call is held by the other user
 
     def __del__(self):
@@ -46,41 +46,47 @@ class CallControl:
         self.__listener_stop = True
         self._listener.join()
 
+    def should_video_flow(self):
+        return not (self.own_hold or self.foreign_hold)
+
     def _listen(self):
         """
         Function that is executed by the listener. Checks if the call must be held, resumed or ended
         """
         # TODO: informar a la interfaz de lo que va pasando
         while not self.__listener_stop:
-            response = self.connection.recv(BUFFER_SIZE).decode().split()
+            try:
+                response = self.connection.recv(BUFFER_SIZE).decode().split()
+            except socket.timeout:  # This exception may only happen if the other user does not answer to our call
+                # TODO: informar que el otro no nos responde
+                break
             if response:
-                if response[0] == "CALL_HOLD":
+                if response[0] == "CALL_ACCEPTED":
+                    self.dst_user.update_udp_port(int(response[2]))
+                    self.connection.settimeout(None)  # The connection should not be closed until wanted
+                    self.own_hold = False  # Start sending video
+                elif response[0] == "CALL_DENIED":
+                    # TODO
+                    break
+                elif response[0] == "CALL_BUSY":
+                    # TODO
+                    break
+                elif response[0] == "CALL_HOLD":
                     self.foreign_hold = False
                 elif response[0] == "CALL_RESUME":
                     self.foreign_hold = True
                 elif response[0] == "CALL_END":
                     # TODO
-                    pass
+                    break
 
     def call_start(self):
         """
-        Calls the user, waits for his answer and updates variables depending on the answer's value. If call is accepted,
-        dst_user's udp port is updated and listener thread is started.
+        Calls the user and runs listener thread
         """
         string_to_send = f"CALLING {self.src_user.nick} {self.src_user.tcp_port}"
         self.connection.send(string_to_send.encode())
-
         self.connection.settimeout(30)  # Sets timeout long enough so the user is able to answer TODO: 30 segs es mucho o poco???
-        response = self.connection.recv(BUFFER_SIZE).decode().split()
-
-        if response[0] == "CALL_ACCEPTED":
-            self.dst_user.update_udp_port(int(response[2]))
-            self.connection.settimeout(None)  # The connection should not be closed until wanted
-            self._listener.start()
-        elif response[0] == "CALL_DENIED":
-            raise CallDenied(self.dst_user.nick)  # TODO: tanto esta como la siguiente excepcion posiblemente no tengan senitdo. Hay que ver como informar a la interfaz
-        elif response[0] == "CALL_BUSY":
-            raise CallBusy(self.dst_user.nick)
+        self._listener.start()
 
     def call_accept(self):
         """
@@ -89,6 +95,7 @@ class CallControl:
         """
         string_to_send = f"CALL_ACCEPTED {self.src_user.nick} {self.src_user.udp_port}"
         self.connection.send(string_to_send.encode())
+        self.own_hold = False  # Start sending video
         self._listener.start()
 
     def call_deny(self):
@@ -126,7 +133,7 @@ class ControlDispatcher:
     def __init__(self, callback):
         self.src_user = CurrentUser.currentUser
         self.sock = _open_tcp_socket(self.src_user)
-        self._current_call_control = None
+        self.current_call_control = None
         self.__call_control_lock = Lock()
         self._listener = threading.Thread(target=self._listen)
         self.__listener_stop = False
@@ -136,22 +143,18 @@ class ControlDispatcher:
     def __del__(self):
         self.__listener_stop = True
         self._listener.join()
-        if self._current_call_control:
-            del self._current_call_control
-
-    def get_call_control(self) -> CallControl:
-        with self.__call_control_lock:
-            return self._current_call_control
+        if self.current_call_control:
+            del self.current_call_control
 
     def set_call_control(self, call_control: CallControl):
         with self.__call_control_lock:
-            self._current_call_control = call_control
+            self.current_call_control = call_control
 
     def del_call_control(self):
         with self.__call_control_lock:
-            if self._current_call_control:
-                del self._current_call_control
-                self._current_call_control = None
+            if self.current_call_control:
+                del self.current_call_control
+                self.current_call_control = None
 
     def _listen(self):
         """
@@ -169,7 +172,7 @@ class ControlDispatcher:
                 response = connection.recv(BUFFER_SIZE).decode().split()
 
                 with self.__call_control_lock:  # Block call control until resolving request
-                    if self._current_call_control:  # If a call control exists, the user is busy
+                    if self.current_call_control:  # If a call control exists, the user is busy
                         connection.send("CALL_BUSY".encode())
                         continue
 
@@ -181,15 +184,14 @@ class ControlDispatcher:
                                          tcp_port=client_address[1],
                                          ip=client_address[0],
                                          udp_port=int(response[2]))
-                    # TODO: informar interfaz llamada entrante y si el usuario la deniega etc
                     connection.settimeout(None)  # The connection should not be closed until wanted
-                    self._current_call_control = CallControl(incoming_user, connection)
+                    self.current_call_control = CallControl(incoming_user, connection)
                     accept = self.callback(incoming_user.nick, incoming_user.ip)
                     if accept:
-                        self._current_call_control.call_accept()
+                        self.current_call_control.call_accept()
                     else:
-                        self._current_call_control.call_deny()
-                        del self._current_call_control
-                        self._current_call_control = None
+                        self.current_call_control.call_deny()
+                        del self.current_call_control
+                        self.current_call_control = None
             except:
                 connection.send(f"CALL_DENIED {self.src_user.nick}".encode())
--- practica3_client.py
+++ practica3_client.py
@@ -21,14 +21,17 @@ MAX_DATAGRAM_SIZE = 65_507
 class VideoClient(object):
     VIDEO_WIDGET_NAME = "video"
     CONNECT_BUTTON = "Connect"
+    HOLD_BUTTON = "Hold"
+    RESUME_BUTTON = "Resume"
+    END_BUTTON = "End Call"
 
     def receive_video(self):
         while True:
             data, addr = self.receive_socket.recvfrom(MAX_DATAGRAM_SIZE)
-            if self.call_control and addr[0] == self.call_control.dst_user.ip:
+            if self.call_control and addr[0] == self.call_control.dst_user.ip and self.call_control.should_video_flow:
                 udp_datagram = udp_datagram_from_msg(data)
                 if self.udp_buffer.insert(udp_datagram):  # Release semaphore only is data was really inserted
-                    self.semaphore.release()
+                    self.video_semaphore.release()
 
     def capture_and_send_video(self):
         sequence_number = 0
@@ -37,9 +40,9 @@ class VideoClient(object):
             local_frame = self.get_frame()
             # Notify visualization thread
             self.camera_buffer.put(local_frame)
-            self.semaphore.release()
+            self.video_semaphore.release()
             # Compress local frame to send it via the socket
-            if self.call_control:
+            if self.call_control and self.call_control.should_video_flow():
                 success, compressed_local_frame = cv2.imencode(".jpg", local_frame, [cv2.IMWRITE_JPEG_QUALITY, 50])
                 if not success:
                     raise Exception("Error compressing the image")
@@ -51,7 +54,8 @@ class VideoClient(object):
 
                 assert (len(udp_datagram) <= MAX_DATAGRAM_SIZE)
 
-                self.send_socket.sendto(udp_datagram, (self.call_control.dst_user.ip, self.call_control.dst_user.udp_port))
+                self.send_socket.sendto(udp_datagram, (self.call_control.dst_user.ip,
+                                                       self.call_control.dst_user.udp_port))
                 sequence_number += 1
 
     def __init__(self, window_size):
@@ -81,12 +85,12 @@ class VideoClient(object):
         CurrentUser("daniel", "V0", CONTROL_PORT, "asdfasdf", VIDEO_PORT)
         self.dispatcher = ControlDispatcher(self.call_callback)
         self.call_control = None
-        self.semaphore = Semaphore()
+        self.video_semaphore = Semaphore()
         self.camera_buffer = Queue()
+        self.udp_buffer = UDPBuffer()
         self.receiving_thread = Thread(target=self.receive_video, daemon=True)
         self.capture_thread = Thread(target=self.capture_and_send_video, daemon=True)
         self.visualization_thread = Thread(target=self.display_video, daemon=True)
-        self.udp_buffer = UDPBuffer()
         self.receiving_thread.start()
         self.capture_thread.start()
         self.visualization_thread.start()
@@ -112,7 +116,7 @@ class VideoClient(object):
     def display_video(self):
         last_remote_frame = None
         while True:
-            self.semaphore.acquire()
+            self.video_semaphore.acquire()
             # Fetch webcam frame
             try:
                 local_frame = self.camera_buffer.get(block=False)
@@ -124,7 +128,7 @@ class VideoClient(object):
             if not remote_frame:
                 remote_frame = last_remote_frame
             # Show local (and remote) frame
-            if remote_frame:  # TODO: esto ya no tiene sentido no? Siempre debería ser True
+            if remote_frame:
                 last_remote_frame = remote_frame
                 remote_frame = cv2.imdecode(np.frombuffer(remote_frame, np.uint8), 1)
                 margin = 10
@@ -133,7 +137,7 @@ class VideoClient(object):
                 mini_frame = cv2.resize(local_frame, (mini_frame_width, mini_frame_height))
                 remote_frame[-mini_frame_height - margin:-margin, -mini_frame_width - margin:-margin] = mini_frame
                 self.show_video(remote_frame)
-            else:
+            elif not remote_frame or (self.call_control and not self.call_control.should_video_flow()):  # TODO: solucion temporal en caso de que la llamada esté parada
                 self.show_video(local_frame)
 
     def buttons_callback(self, name: str):
@@ -144,14 +148,24 @@ class VideoClient(object):
                 user = User("qwerty", "V0", CONTROL_PORT, ip=remote_ip)
                 self.call_control = CallControl(user)
                 self.dispatcher.set_call_control(self.call_control)
+                self.call_control.call_start()
             except ValueError:
                 pass
+        elif name == VideoClient.HOLD_BUTTON:
+            self.call_control.call_hold()
+            # TODO: intercambiar boton con resume y viceversa
+        elif name == VideoClient.RESUME_BUTTON:
+            self.call_control.call_resume()
+        elif name == VideoClient.END_BUTTON:
+            self.call_control.call_end()
+            self.dispatcher.del_call_control()
+            #self.call_control = None TODO creo que no hace falta
 
     def call_callback(self, username: str, ip: str) -> bool:
         accept = self.gui.yesNoBox("Incoming call",
                                    f"The user {username} is calling from {ip}. Do you want to accept the call?")
         if accept:
-            self.call_control = self.dispatcher.get_call_control()
+            self.call_control = self.dispatcher.current_call_control  # We are protected by the lock, so this is legal
 
         return accept
 
